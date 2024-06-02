a8e970384da313c10e88c6c952e16924186d7dff
Ilya R <twentyny@yandex.ru>
2024-06-02 19:14:12 +0500

    added open(google.com)

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -13,5 +13,6 @@ public class SearchTests {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
+        open("https://www.google.com/");
     }
 }
