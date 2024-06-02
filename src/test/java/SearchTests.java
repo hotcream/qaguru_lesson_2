c8c35d29956cf2faeeeb38775e5809e175278a78
Ilya R <twentyny@yandex.ru>
2024-06-02 19:18:21 +0500

    deleted open(google.com)

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -13,6 +13,5 @@ public class SearchTests {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
-        open("https://www.google.com/");
     }
 }
