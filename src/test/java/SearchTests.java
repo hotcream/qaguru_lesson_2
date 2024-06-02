38a28e045143fda0e9f41f5f88166520853b7599
Ilya R <twentyny@yandex.ru>
2024-06-02 19:25:54 +0500

    edited link google

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selenide.open;
 public class SearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.google.ru/");
+        open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
     }
